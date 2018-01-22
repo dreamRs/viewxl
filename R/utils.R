f0b840c6154849e0a4dc152684fd44746c621883
pvictor <perrier.victor@gmail.com>
2018-01-22 21:04:31 +0100

    view in xl

---
+++ R/utils.R
@@ -0,0 +1,58 @@
+
+#' Retrieve a data.frame by name from an environment
+#'
+#' @param df character, name of the object
+#' @param env an environment
+#'
+#' @return the object
+#' @noRd
+#'
+#' @importFrom utils data
+#'
+get_df <- function(df, env = globalenv()) {
+  if (df %in% ls(name = env)) {
+    get(x = df, envir = env)
+  } else {
+    NULL
+  }
+}
+
+
+
+#' Search for object with specific class in an environment
+#'
+#' @param what a class to look for
+#' @param env An environment
+#'
+#' @return Character vector of the names of objects, NULL if none
+#' @noRd
+#'
+#' @examples
+#' \dontrun{
+#'
+#' search_obj("data.frame")
+#'
+#'
+#' gg <- ggplot()
+#' search_obj("ggplot")
+#'
+#' }
+search_obj <- function(what = "data.frame", env = globalenv()) {
+  all <- ls(name = env)
+  objs <- lapply(
+    X = all,
+    FUN = function(x) {
+      if (inherits(get(x, envir = env), what = what)) {
+        x
+      } else {
+        NULL
+      }
+    }
+  )
+  objs <- unlist(objs)
+  if (length(objs) == 1 && objs == "") {
+    NULL
+  } else {
+    objs
+  }
+}
---
+++ R/view_in_xl.R
@@ -0,0 +1,94 @@
+
+
+#' Open a data.frame in 'Excel'
+#'
+#' @param df the name of a \code{data.frame}, if not provided, an addin is launched.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Launch addin
+#' view_in_xl()
+#'
+#' # or pass the name of a data.frame
+#' view_in_xl("iris")
+#'
+#' }
+#' @importFrom miniUI miniPage miniButtonBlock miniContentPanel
+#' @importFrom rstudioapi getActiveDocumentContext
+#' @importFrom shiny actionLink actionButton icon observeEvent
+#'  runGadget dialogViewer tags stopApp
+#' @importFrom utils browseURL
+#' @importFrom writexl write_xlsx
+view_in_xl <- function(df = NULL) {
+  if (is.null(df)) {
+    context <- getActiveDocumentContext()
+    df <- context$selection[[1]]$text
+    is_df <- tryCatch({
+      test <- get(df, envir = .GlobalEnv)
+      list(res = is.data.frame(test))
+    }, error = function(e) {
+      list(res = FALSE)
+    })
+    if (!is_df$res) {
+      df <- search_obj()
+    }
+  }
+  if (length(df) == 0) {
+    message("It seems that there are no data.frames in global environment...")
+    return(invisible())
+  }
+  if (length(df) == 1) {
+    tmp <- tempfile(fileext = ".xlsx")
+    write_xlsx(x = get_df(df), path = tmp)
+    browseURL(url = tmp)
+    return(invisible(tmp))
+  } else {
+    tags_df <- lapply(
+      X = df,
+      FUN = function(x) {
+        obj <- get_df(x)
+        tags$li(
+          actionLink(
+            inputId = paste0("view_in_xl_", x),
+            label = sprintf("%s (%s obs. of  %s variables)", x, nrow(obj), ncol(obj))
+          )
+        )
+      }
+    )
+    ui <- miniPage(
+      miniContentPanel(
+        tags$ul(tags_df)
+      ),
+      miniButtonBlock(
+        actionButton(
+          inputId = "close", label = "Close",
+          icon = icon("remove"),
+          class = "btn-block btn-primary"
+        )
+      )
+    )
+    server <- function(input, output, session) {
+      lapply(
+        X = paste0("view_in_xl_", df),
+        FUN = function(x) {
+          observeEvent(input[[x]], {
+            tmp <- tempfile(fileext = ".xlsx")
+            obj <- gsub(pattern = "view_in_xl_", replacement = "", x = x)
+            write_xlsx(x = get_df(obj), path = tmp)
+            browseURL(url = tmp)
+          }, ignoreInit = TRUE)
+        }
+      )
+      observeEvent(input$close, stopApp())
+    }
+    inviewer <- dialogViewer(
+      "View a data.frame in Excel",
+      width = 450, height = 180
+    )
+    runGadget(app = ui, server = server, viewer = inviewer)
+  }
+}
+
